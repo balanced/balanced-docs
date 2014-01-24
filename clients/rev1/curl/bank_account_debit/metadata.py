b6da34d44b3d26694a32bbb6cced0d5c05e397ac
Ben Mills <ben@unfiniti.com>
2014-01-24 09:59:25 -0800

    Fix bank account debit scenario endpoint

--- clients/rev1/curl/bank_account_debit/metadata.py
+++ clients/rev1/curl/bank_account_debit/metadata.py
@@ -2,10 +2,10 @@ bav_response = json.loads(
     storage['bank_account_verification_update']['response']
 )
 
-bank_account_href = bav_response['links']['bank_account_verifications.bank_account'].replace('{bank_account_verifications.bank_account}', bav_response['bank_account_verifications'][0]['links']['bank_account']) + '/debits'
+bank_account_href = bav_response['links']['bank_account_verifications.bank_account'].replace('{bank_account_verifications.bank_account}', bav_response['bank_account_verifications'][0]['links']['bank_account'])
 
 request = {
-    'uri': bank_account_href,
+    'uri': bank_account_href + '/debits',
     'payload': {
         'amount': 5000,
         'appears_on_statement_as': 'Statement text',
