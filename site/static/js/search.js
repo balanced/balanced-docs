06947bc5081b2d7dd328a75946127cd139d62b76
Richie <rserna2010@gmail.com>
2014-03-17 21:10:16 -0700

    Add js to ensure query input stays filled

--- site/static/js/search.js
+++ site/static/js/search.js
@@ -2,13 +2,24 @@
 // search.js //
 ///////////////
 
-$('.search link').attr('href','/static/css/styles.css')
+$('.search link').attr('href','/static/css/styles.css');
+
 
 $( document ).ready(function() {
     setTimeout(function(){
         $('.gsc-input').attr('background-color', '#59606B')
         $('#gsc-i-id1').attr('style', "width: 100%; padding: 0px; border: none; margin: -0.0625em 0px 0px; height: 1.25em; outline: none; background-color: #59606B; background-position: 0% 50%; background-repeat: no-repeat no-repeat;")
-    }, 100)
+        $('input#gsc-i-id1').blur(function () {
+            var searchTerm = $('input#gsc-i-id1').val();
+            if (searchTerm.length != 0) {
+                setTimeout(function () {
+                    $('input#gsc-i-id1').val(searchTerm)
+                }, 140)
+            }
+        });
+    }, 100);
 });
+
+
 
 
