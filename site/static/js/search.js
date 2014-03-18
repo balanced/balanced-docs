abb7cae494b3f7719b9e01d2bb57ee6c1083db5c
Richie <rserna2010@gmail.com>
2014-03-17 21:25:18 -0700

    Add sessionStorage to persist search queries

--- site/static/js/search.js
+++ site/static/js/search.js
@@ -11,12 +11,14 @@ $( document ).ready(function() {
         $('#gsc-i-id1').attr('style', "width: 100%; padding: 0px; border: none; margin: -0.0625em 0px 0px; height: 1.25em; outline: none; background-color: #59606B; background-position: 0% 50%; background-repeat: no-repeat no-repeat;")
         $('input#gsc-i-id1').blur(function () {
             var searchTerm = $('input#gsc-i-id1').val();
+            sessionStorage.setItem('query',searchTerm)
             if (searchTerm.length != 0) {
                 setTimeout(function () {
                     $('input#gsc-i-id1').val(searchTerm)
                 }, 140)
             }
         });
+        $('input#gsc-i-id1').val(sessionStorage.getItem('query'))
     }, 100);
 });
 
