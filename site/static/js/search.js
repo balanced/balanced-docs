db1a43eddff65cc9b33510761bad89398633c713
Richie <rserna2010@gmail.com>
2014-03-27 12:51:18 -0700

    Add watermark removal class

--- site/static/js/search.js
+++ site/static/js/search.js
@@ -7,8 +7,8 @@ $('.search link').attr('href','/static/css/styles.css');
 
 $( document ).ready(function() {
     setTimeout(function(){
-        $('.gsc-input').attr('background-color', '#59606B')
-        $('#gsc-i-id1').attr('style', "width: 100%; padding: 0px; border: none; margin: -0.0625em 0px 0px; height: 1.25em; outline: none; background-color: #59606B; background-position: 0% 50%; background-repeat: no-repeat no-repeat;")
+        $('.gsc-input').attr('background-color', '#59606B');
+        $('#gsc-i-id1').addClass('google-remove-watermark');
         $('input#gsc-i-id1').blur(function () {
             var searchTerm = $('input#gsc-i-id1').val();
             sessionStorage.setItem('query',searchTerm)
