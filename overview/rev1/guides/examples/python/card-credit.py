c018cf59a2a01093eafa472523ee6b4b1c9dbeb8
Ben Mills <ben@unfiniti.com>
2014-05-22 10:49:18 -0600

    Include card fetch in card credit examples

--- overview/rev1/guides/examples/python/card-credit.py
+++ overview/rev1/guides/examples/python/card-credit.py
@@ -1,3 +1,5 @@
+# card_href is the stored href for the Card
+card = balanced.Card.fetch(card_href)
 card.credit(
   appears_on_statement_as='Some text',
   amount=5000,
--- overview/rev1/guides/examples/ruby/card-credit.rb
+++ overview/rev1/guides/examples/ruby/card-credit.rb
@@ -1,3 +1,5 @@
+# card_href is the stored href for the Card
+card = Balanced::Card.fetch(card_href)
 card.credit(
   :amount => 5000,
   :appears_on_statement_as => 'Some text',
