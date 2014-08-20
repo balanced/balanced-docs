e0917eacba37b79aefccdea1d86890760d8894b7
richie serna <richie@balancedpayments.com>
2014-08-20 13:10:37 -0700

    Use token

--- clients/rev1/curl/snippets/order-credit-marketplace.sh
+++ clients/rev1/curl/snippets/order-credit-marketplace.sh
@@ -4,12 +4,12 @@ curl https://api.balancedpayments.com/marketplaces \
    -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL:
 
 # Determine owner customer from marketplaces.owner_customer link and get its bank accounts
-curl https://api.balancedpayments.com/customers/CU1U8FEqP5FsisYD0D5G6aS4/bank_accounts \
+curl https://api.balancedpayments.com/customers/:customer_id/bank_accounts \
    -H "Accept: application/vnd.api+json;revision=1.1" \
    -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL:
 
-# Determine the debits href from the bank_accounts.debits link and create a credit
-curl https://api.balancedpayments.com/bank_accounts/BA1VKlrw3m7lNyouaU5h8Xba/credits \
+# Use the bank account id to create a credit
+curl https://api.balancedpayments.com/bank_accounts/:bank_account_id/credits \
    -H "Accept: application/vnd.api+json;revision=1.1" \
    -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL: \
    -d "amount=2000"
