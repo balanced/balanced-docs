13d8127f7386b88f2076a09d204c826c9647dfca
richie serna <richie@balancedpayments.com>
2014-08-21 14:37:48 -0700

    Add order param to credit and debit scenarios

--- clients/rev1/curl/snippets/bank-account-debit.sh
+++ clients/rev1/curl/snippets/bank-account-debit.sh
@@ -1,6 +1,9 @@
-curl https://api.balancedpayments.com/bank_accounts/BA2akihQKCAhhkX5aecSubr2/debits \
+# :bank_account_id is the stored id for the BankAccount
+# :order_href is the stored href for the Order
+curl https://api.balancedpayments.com/bank_accounts/bank_account_id/debits \
    -H "Accept: application/vnd.api+json;revision=1.1" \
    -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL: \
    -d "appears_on_statement_as=Statement text" \
    -d "amount=5000" \
-   -d "description=Some descriptive text for the debit in the dashboard"
+   -d "description=Some descriptive text for the debit in the dashboard" \
+   -d "order=:order_href"
--- clients/rev1/curl/snippets/reversal-create.sh
+++ clients/rev1/curl/snippets/reversal-create.sh
@@ -1,4 +1,5 @@
 # :credit_id is the stored id for the Credit
+# :order_href is the stored href for the Order
 curl https://api.balancedpayments.com/credits/:credit_id/reversals \
      -H "Accept: application/vnd.api+json;revision=1.1" \
      -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL: \
@@ -6,4 +7,5 @@ curl https://api.balancedpayments.com/credits/:credit_id/reversals \
      -d "description=Reversal for Order #1111" \
      -d "meta[merchant.feedback]=positive" \
      -d "meta[user.refund_reason]=not happy with product" \
-     -d "meta[fulfillment.item.condition]=OK"
+     -d "meta[fulfillment.item.condition]=OK" \
+     -d "order=:order_href"
