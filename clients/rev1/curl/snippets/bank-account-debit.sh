fb0f74885ea2854931fe5c11ec03cf0428634f90
richie serna <richie@balancedpayments.com>
2014-08-21 14:41:00 -0700

    Fix typo

--- clients/rev1/curl/snippets/bank-account-debit.sh
+++ clients/rev1/curl/snippets/bank-account-debit.sh
@@ -1,6 +1,6 @@
 # :bank_account_id is the stored id for the BankAccount
 # :order_href is the stored href for the Order
-curl https://api.balancedpayments.com/bank_accounts/bank_account_id/debits \
+curl https://api.balancedpayments.com/bank_accounts/:bank_account_id/debits \
    -H "Accept: application/vnd.api+json;revision=1.1" \
    -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL: \
    -d "appears_on_statement_as=Statement text" \
