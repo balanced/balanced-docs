7bb01956391cffd5a7846e2400775268abc5def5
Ben Mills <ben@unfiniti.com>
2014-09-02 14:01:37 -0600

    Track lang as a custom dimension in Google Analytics

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -4,4 +4,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-30733850-5', 'balancedpayments.com');
-ga('send', 'pageview');
+ga('send', 'pageview', {
+  'dimension1': ($.cookie('language') != null) ? $.cookie('language') : 'bash'
+});
