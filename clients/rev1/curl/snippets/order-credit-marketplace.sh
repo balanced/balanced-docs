1785738e87f16fbc9e5cccf04dd531f36eca05f9
richie serna <richie@balancedpayments.com>
2014-09-26 16:52:30 -0700

    Edit order credit marketplace snippet

--- clients/rev1/curl/snippets/order-credit-marketplace.sh
+++ clients/rev1/curl/snippets/order-credit-marketplace.sh
@@ -9,7 +9,9 @@ curl https://api.balancedpayments.com/customers/:customer_id/bank_accounts \
    -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL:
 
 # Use the bank account id to create a credit
+# Use the order id to associate the credit with the order
 curl https://api.balancedpayments.com/bank_accounts/:bank_account_id/credits \
    -H "Accept: application/vnd.api+json;revision=1.1" \
    -u ak-test-h7F8F3u41y6LzCK4nZeVd5BafaWOUuZL: \
-   -d "amount=2000"
+   -d "amount=2000" \
+   -d "order=/orders/:order_id"
