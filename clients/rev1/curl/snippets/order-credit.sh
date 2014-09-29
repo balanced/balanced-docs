5a09d4fe54afbe8c86a9ef0fd0e6fc9a9617ae55
richie serna <richie@balancedpayments.com>
2014-09-29 15:06:23 -0700

    Add order to credit merchant example

--- clients/rev1/curl/snippets/order-credit.sh
+++ clients/rev1/curl/snippets/order-credit.sh
@@ -1,6 +1,8 @@
 # credit from the order
 # bank_account_id: the id of a bank_account to be credited
+# order_id: the id of the order to be credited
 curl https://api.balancedpayments.com/bank_accounts/:bank_account_id/credits \
      -H "Accept-Type: application/vnd.api+json;revision=1.1" \
      -u ak-test-1sKqYrBZG6WYpHphDAsM7ZXFEmJlAn1GE: \
-     -d "amount=8000"
+     -d "amount=8000" \
+     -d "order=/orders/:order_id"
